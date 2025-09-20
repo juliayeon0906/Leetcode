f64a2c5ebda03558976aedcef66ab1c56f1dda1e
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-09-20 15:04:48 -0300

    Time: 7 ms (98.50%) | Memory: 46.5 MB (86.43%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        List<int[]> res = new ArrayList<>();
+        int[] cur = intervals[0];
+
+        for(int i = 1; i < intervals.length; i++) {
+            if(cur[1] >= intervals[i][0]){
+                cur[1] = Math.max(cur[1], intervals[i][1]);
+            } else {
+                res.add(cur);
+                cur = intervals[i];
+            }
+        }
+        res.add(cur);
+
+        return res.toArray(new int[res.size()][]);
+
+    }
+}
