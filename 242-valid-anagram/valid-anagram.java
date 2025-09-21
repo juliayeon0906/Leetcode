5f60e55722629ccfe916c7990cab1a51582606c1
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-09-21 14:08:46 -0300

    Time: 26 ms (5.76%) | Memory: 42.7 MB (97.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,30 +5,15 @@ class Solution {
         }
 
         Map<Character, Integer> map = new HashMap<>();
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-
+        
         for(int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
         }
 
-        for(Character c : t.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c, map.get(c) - 1);
-            } else {
-                return false;
-            }
+        for (int count: map.values()){
+            if(count != 0) return false;
         }
-
-        boolean allZero = true;
-
-        for(int val : map.values()) {
-            if(val != 0) {
-                allZero = false;
-                break;
-            }
-        }
-
-        return allZero;
+        return true;
     }
 }
