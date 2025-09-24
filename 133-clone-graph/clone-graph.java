ee60d6fe7561fdf8916edd55622203eb7e7fe68c
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-09-24 14:22:24 -0300

    Time: 25 ms (67.32%) | Memory: 42.9 MB (36.41%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -19,33 +19,25 @@ class Node {
 */
 
 class Solution {
-    Map<Node, Node> nodes = new HashMap<>();
+    Map<Node, Node> map = new HashMap<>();
 
     public Node cloneGraph(Node node) {
-        // If the starting node is null, the clone must also be null.
-        if (node == null) return null;
-
-        // Check if the node has already been cloned.
-        // The HashMap.containsKey() method checks for the key's existence.
-        if (nodes.containsKey(node)) {
-            // If it's already cloned, return the existing copy from the map.
-            return nodes.get(node);
+        if(node == null) {
+            return null;
         }
 
-        // This is the first time visiting this node.
-        // Create a new node (the copy).
-        Node copiedNode = new Node(node.val);
-        // Add the newly created copy to the map to prevent re-cloning.
-        nodes.put(node, copiedNode);
-
-        // Iterate through all neighbors of the original node.
-        for (Node neighbor : node.neighbors) {
-            // Recursively call cloneGraph for each neighbor and add the
-            // returned clone to our new node's neighbor list.
-            copiedNode.neighbors.add(cloneGraph(neighbor));
+        if(map.containsKey(node)) { // prevents the re-cloning
+        // if the node was already cloned, return the clone
+            return map.get(node);
         }
 
-        // Return the new node, whose adjacency list is now fully copied.
-        return copiedNode;
+        Node copied = new Node(node.val);
+        map.put(node, copied); // save the copied nodes we've seen so far
+
+        for(Node neighbor : node.neighbors) {
+            copied.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return copied;
     }
 }
