d75fae84ebd8fa3ea38b485e8604b908dce7e621
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-10-11 15:48:43 -0300

    Time: 2 ms (92.99%) | Memory: 47.1 MB (81.03%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
+        // since input is sorted -> sorting is unnecessary
+        // Using the two pointers (1: beginning of the array, 2: end of the array)
+        int ptr1 = 0;
+        int ptr2 = numbers.length - 1;
 
-        while(left < right) {
-            int total = numbers[left] + numbers[right];
-            if(left < right && total > target) {
-                right--;
-            } else if(left < right && total < target) {
-                left++;
-            } else {
-                return new int[]{left+1, right+1};
+        // iterate arr until first pointer is greater than equal to second ptr
+        while(ptr1 < ptr2) {
+            // if the sum is greater than target -> decrease ptr2
+            int sum = numbers[ptr1] + numbers[ptr2];
+            if(sum > target) {
+                ptr2--;
+            } else if(sum < target) {            // if the sum is less -> increase ptr1
+                ptr1++;
+            } else { // if the sum is equal -> return the ptr to int array
+                return new int[]{ptr1 + 1, ptr2 + 1};
             }
         }
+
+        // return empty otherwise
         return new int[]{};
     }
 }
