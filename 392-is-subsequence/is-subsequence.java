0794e8062fe75ac102732ea7caa90b51c4483e83
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-10-11 15:38:37 -0300

    Time: 1 ms (93.05%) | Memory: 41.5 MB (74.71%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -3,12 +3,14 @@ class Solution {
         // using two pointer
         int ptrS = 0;
         int ptrT = 0;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
 
         // iterate the characters
         while(ptrT < t.length()){
             // find the matched character in string t
             // if found, we move the ptr S to find next
-            if(ptrS < s.length() && s.charAt(ptrS) == t.charAt(ptrT)) {
+            if(ptrS < s.length() && arr1[ptrS] == arr2[ptrT]) {
                 ptrS++;
             }
 
