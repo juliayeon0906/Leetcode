24d65b1d969c9e8dfd2e26dbe292c47571b700fd
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-10-11 15:36:45 -0300

    Time: 2 ms (66.59%) | Memory: 41.3 MB (95.71%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        // using two pointer
         int ptrS = 0;
         int ptrT = 0;
 
-        while(ptrS < s.length() && ptrT < t.length()) {
-            if(s.charAt(ptrS) == t.charAt(ptrT)) {
+        // iterate the characters
+        while(ptrT < t.length()){
+            // find the matched character in string t
+            // if found, we move the ptr S to find next
+            if(ptrS < s.length() && s.charAt(ptrS) == t.charAt(ptrT)) {
                 ptrS++;
             }
+
             ptrT++;
         }
+            
+        // return boolean (if the ptr S is pointing to the end of the character in string s)
         return ptrS == s.length();
     }
 }
