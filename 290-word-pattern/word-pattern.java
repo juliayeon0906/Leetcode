177dfe90b061d84c014c970304e871af49d4df46
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-10-22 14:56:07 -0300

    Time: 1 ms (80.04%) | Memory: 42 MB (10.44%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,32 +1,29 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if(pattern.length() != words.length){
+        String[] word = s.split(" ");
+        if(pattern.length() != word.length) {
             return false;
         }
 
-        Map<Character, String> p2w = new HashMap<>();
-        Map<String, Character> w2p = new HashMap<>();
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
 
-        for(int i = 0; i < pattern.length(); i++){
-            char ch = pattern.charAt(i);
-            String word = words[i];
-            if(p2w.containsKey(ch)){
-                if(!p2w.get(ch).equals(word)) {
-                    return false;
-                }
-            } else {
-                p2w.put(ch, word);
+        for(int i = 0; i < pattern.length(); i++) {
+            char patChar = pattern.charAt(i);
+            if(map1.containsKey(patChar) && !map1.get(patChar).equals(word[i])) {
+                return false;
             }
-
-            if(w2p.containsKey(word)){
-                if(w2p.get(word) != ch) {
-                    return false;
-                }
-            } else {
-                w2p.put(word, ch);
-            }
+            map1.put(patChar, word[i]);
         }
+
+        for(int i = 0; i < word.length; i++) {
+            char patChar = pattern.charAt(i);
+            if(map2.containsKey(word[i]) && map2.get(word[i]) != patChar){
+                return false;
+            }
+            map2.put(word[i], patChar);
+        }
+
         return true;
     }
 }
