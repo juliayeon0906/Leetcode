6cc081a870766faa98dee0c5a569c1774d0fdc71
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-12 11:29:14 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (25.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,15 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        midx = m - 1
-        nidx = n - 1
-        right = m + n - 1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-        while nidx >= 0:
-            if midx >= 0 and nums1[midx] > nums2[nidx]:
-                nums1[right] = nums1[midx]
-                midx -= 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1[right] = nums2[nidx]
-                nidx -= 1
-            right -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
