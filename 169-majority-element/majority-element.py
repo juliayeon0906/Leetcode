2941f805c7b72d885a90eb24156025981720228b
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-13 15:46:16 -0400

    Time: 11 ms (29.40%) | Memory: 19.2 MB (86.85%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hash = {}
-        res = majority = 0
+
+        majority = res = 0
 
         for n in nums:
-            hash[n] = 1 + hash.get(n,0)
+            hash[n] = 1 + hash.get(n, 0)
             if hash[n] > majority:
                 res = n
                 majority = hash[n]
-        
+
         return res
-            
