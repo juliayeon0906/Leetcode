d00732e79de01107b0c3086fc7de45dee2a7cb53
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-13 16:00:25 -0400

    Time: 3 ms (26.16%) | Memory: 17.9 MB (24.63%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        sp = tp = 0
+        s_ptr = t_ptr = 0
 
-        while sp < len(s) and tp < len(t):
-            if s[sp] == t[tp]:
-                sp += 1
-            tp += 1
+        while s_ptr < len(s) and t_ptr < len(t):
+            if s[s_ptr] == t[t_ptr]:
+                s_ptr += 1
+            t_ptr += 1
         
-        return sp == len(s)
+        return s_ptr == len(s)
