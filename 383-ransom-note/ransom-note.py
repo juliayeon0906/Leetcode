7124dbb8a30aa1fce6bb441becb98b400d1d4be8
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-14 12:59:26 -0400

    Time: 20 ms (51.47%) | Memory: 18 MB (53.93%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,18 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # {} -> magazine char : count #
-        # if ransomNote char contained in {} && count > 0 -> count--
-        # else: return false
-        # end: return true
+        hash = {}
 
-        map = {}
-
-        for char in magazine:
-            map[char] = 1 + map.get(char, 0)
+        for c in magazine:
+            hash[c] = 1 + hash.get(c, 0)
         
-        for char in ransomNote:
-            if char not in map or map[char] <= 0:
+        for c in ransomNote:
+            if c not in hash or hash[c] <= 0:
                 return False
-            map[char] -= 1
+            hash[c] -= 1
 
         return True
