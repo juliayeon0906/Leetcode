507ba5ad02a0c8a66ab575bc35335776ec04a053
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-14 13:09:53 -0400

    Time: 4 ms (74.82%) | Memory: 17.8 MB (95.43%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -6,15 +6,11 @@ class Solution:
         seen = {}
 
         for s_char, t_char in zip(s, t):
-            key_s = ("s_char", s_char)
-            key_t = ("t_char", t_char)
-
-            if key_s in seen and seen[key_s] != t_char:
+            if s_char in seen:
+                if seen[s_char] != t_char:
+                    return False
+            elif t_char in seen.values():
                 return False
-            if key_t in seen and seen[key_t] != s_char:
-                return False
-            
-            seen[key_s] = t_char
-            seen[key_t] = s_char
+            seen[s_char] = t_char
         
         return True
