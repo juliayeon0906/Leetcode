65762f4f5600f07d737014b720a0df510c54165e
juliayeon0906 <99053704+juliayeon0906@users.noreply.github.com>
2025-11-23 20:27:44 -0400

    Time: 547 ms (39.73%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-SELECT DISTINCT l1.num AS ConsecutiveNums
-FROM Logs l1, Logs l2, Logs l3
-WHERE l1.num = l2.num
-    AND l2.num = l3.num
-    AND l1.id = l2.id - 1
-    AND l2.id = l3.id - 1;
+WITH cte AS (
+    SELECT num,
+    lead(num,1) OVER() num1,
+    lead(num,2) OVER() num2
+    FROM Logs
+)
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM cte
+WHERE (num=num1)
+    AND (num=num2)
